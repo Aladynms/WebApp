ec3dd2323a75aedb968baad0c3a8f8864f176c88
Michał Sala <salamichal99@gmail.com>
2025-06-19 23:49:31 +0200

    kanban3

--- WebApp/src/views/StoryView.vue
+++ WebApp/src/views/StoryView.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import { StoryService } from "@/api/StoryService";
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import TaskForm from "@/components/TaskForm.vue";
 import TaskKanban from "@/components/TaskKanban.vue";
 
 const route = useRoute();
 const storyId = Number(route.params.id);
 const story = ref(StoryService.getAll().find(s => s.id === storyId) || null);
+
+// Przeładuj TaskKanban przez zmianę klucza
+const kanbanKey = ref(0);
+const refreshKanban = () => kanbanKey.value++;
 </script>
 
 <template>
@@ -15,7 +19,7 @@ const story = ref(StoryService.getAll().find(s => s.id === storyId) || null);
     <h1 class="text-2xl font-bold mb-2">Historyjka: {{ story?.name }}</h1>
     <p class="mb-4">{{ story?.description }}</p>
 
-    <TaskForm :story-id="storyId" @updated="() => {}" />
-    <TaskKanban :story-id="storyId" />
+    <TaskForm :story-id="storyId" @updated="refreshKanban" />
+    <TaskKanban :story-id="storyId" :key="kanbanKey" />
   </div>
 </template>
